0a3fa44571f901d0f09066a3809b5f3f4cb5136b
Peter Hansen <phansen@physics.umn.edu>
2014-07-24 22:30:38 -0500

    p14

---
+++ p14.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def collatz(n):
+   if n % 2:
+      return 3*n +1
+   else:
+      return n/2
+
+
+
+collatz_length = {1:1}
+
+for n in range(2,1000000):
+   chain = [n]
+   collatz_length[n]=1
+   i = collatz(n)
+   while not i in collatz_length:
+      chain.append(i)
+      collatz_length[i] = 0
+      for c in chain:
+         collatz_length[c] += 1
+      i = collatz(i)
+   else:
+      for c in chain:
+         collatz_length[c] += collatz_length[i]
+
+print collatz_length.keys()[np.argmax(collatz_length.values())]
+
