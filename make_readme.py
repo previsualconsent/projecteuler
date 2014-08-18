cc99361c4c5ab677f4a1312ad4d86278c9b60a3c
Peter Hansen <phansen@physics.umn.edu>
2014-08-18 12:10:32 -0500

    renamed files

--- make_readme.py
+++ make_readme.py
@@ -8,7 +8,7 @@ f = open("README.md","w")
 f.write("## Times for Each Problem\n\n")
 for i in range(1, int(sys.argv[1])+1):
    t = time()
-   call(["python", "p%i.py"%i])
+   call(["python", "p%03d.py"%i])
    f.write("%i.  %.3f seconds\n" %(i, time()-t))
 
 f.close()
